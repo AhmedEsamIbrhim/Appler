272c60fe7b6f728ce68ba88f3ba58801e16e1f42
unknown <medo1412005@gmail.com>
2022-02-15 13:56:04 +0200

    Settings Box

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Start Setting Box
+let settingsBox = document.querySelector(".settings-box");
+let gear        = document.querySelector(".settings-box .gear");
+function open_settings_box(button) {
+    if(button) {
+        button.addEventListener('click', (e) => {
+            settingsBox.classList.toggle('open');
+        });
+    }
+}
+open_settings_box(gear);
+// Start Set Main Color Of Website
+let listColor = document.querySelector(".settings-box .setting-body .colors .colors-list");
+let allColors = document.querySelectorAll(".settings-box .setting-body .colors .colors-list li");
+allColors.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const color = e.target.dataset.color;
+        localStorage.setItem('main-color', color);
+        document.body.style.setProperty('--main-color',localStorage.getItem('main-color'));
+    });
+    document.body.style.setProperty('--main-color',localStorage.getItem('main-color'));
+});
 // Start Send From 
 let url      = 'prossed.php';
 let btn_sub  = document.querySelector(".send-form");
